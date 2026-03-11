da56c48a77f6ce8621c84049a361795e312b9892
夜影、NightShadow <169712528+1224HuangJin@users.noreply.github.com>
2026-03-11 14:13:59 +0100

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+microshield.Servo(microshield.Servos.S0, 0)
+tinkercademy.LED(DigitalPin.P0, OnOff.On)
+tinkercademy.LED(DigitalPin.P1, OnOff.Off)
+tinkercademy.LED(DigitalPin.P2, OnOff.Off)
 basic.forever(function () {
 	
 })
