9be7df91b77ce5cdad177538911c4cc20074fcf5
夜影、NightShadow <169712528+1224HuangJin@users.noreply.github.com>
2026-05-13 14:13:23 +0200

    MINIMO SE VA

--- main.ts
+++ main.ts
@@ -1,31 +1,14 @@
-basic.showString("HOLA")
 microshield.Servo(microshield.Servos.S0, 0)
 tinkercademy.LED(DigitalPin.P12, OnOff.On)
-tinkercademy.LED(DigitalPin.P14, OnOff.Off)
 basic.forever(function () {
-    if (pins.digitalReadPin(DigitalPin.P0) == 0) {
+    if (pins.digitalReadPin(DigitalPin.P1) == 0) {
         microshield.Servo(microshield.Servos.S0, 90)
-        tinkercademy.LED(DigitalPin.P12, OnOff.Off)
-        tinkercademy.LED(DigitalPin.P14, OnOff.On)
-        basic.showLeds(`
-            . . # . .
-            . # # # .
-            # . # . #
-            . . # . .
-            . . # . .
-            `)
-        basic.pause(5000)
-        tinkercademy.LED(DigitalPin.P14, OnOff.Off)
         tinkercademy.LED(DigitalPin.P12, OnOff.On)
-        basic.showLeds(`
-            . # # # .
-            # . . # #
-            # . # . #
-            # # . . #
-            . # # # .
-            `)
-        basic.pause(1000)
-        microshield.Servo(microshield.Servos.S0, 0)
+        tinkercademy.LED(DigitalPin.P14, OnOff.Off)
     }
-    basic.pause(500)
+    basic.pause(5000)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    microshield.Servo(microshield.Servos.S0, 0)
+    tinkercademy.LED(DigitalPin.P14, OnOff.On)
+    tinkercademy.LED(DigitalPin.P12, OnOff.Off)
 })
